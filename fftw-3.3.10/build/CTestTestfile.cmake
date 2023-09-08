01200902f470e558f95073a9b4493592932cb3b9
Ryota Shimokawara <Ryota.Shimokawara@gmail.com>
2023-09-08 09:17:18 +0900

    [add] For static build

---
+++ fftw-3.3.10/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/simom/OneDrive/デスクトップ/SpatialAudioEngine_work/fftw-3.3.10
+# Build directory: C:/Users/simom/OneDrive/デスクトップ/SpatialAudioEngine_work/fftw-3.3.10/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
